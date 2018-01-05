c074522012f5367d1922d739db3fb2347fef2141
jb628 <jon.buch3@gmail.com>
2018-01-04 23:39:14 -0500

    add ajax call

--- src/assets/main.js
+++ src/assets/main.js
@@ -3,7 +3,9 @@ $(function() {
     url:'https://www.codeschool.com/users/jb628.json',
     dataType: 'jsonp',
     success: function (response) {
-
+      for (course of courses.completed) {
+        $(#badges).append("<div class='course'>/<div>")
+      }
     }
   });
   // your code will go here
