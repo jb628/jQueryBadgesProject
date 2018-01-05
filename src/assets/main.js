7c750c3b87bb9c8fb5ef72c38797530c96a57efa
jb628 <jon.buch3@gmail.com>
2018-01-05 00:16:26 -0500

    finish badge page

--- src/assets/main.js
+++ src/assets/main.js
@@ -3,11 +3,38 @@ $(function() {
     url:'https://www.codeschool.com/users/jb628.json',
     dataType: 'jsonp',
     success: function (response) {
-      for (course of courses.completed) {
-        $(#badges).append("<div class='course'>/<div>")
-      }
+      console.log('response', response.courses.completed);
+      addCourses(response.courses.completed);
     }
   });
   // your code will go here
+  function addCourses(courses){
+
+    var $badges = $('#badges');
+
+    courses.forEach(function(course) {
+
+      var $course = $('<div />', {
+        'class': 'course'
+      }).appendTo($badges);
+
+      $('<h3 />', {
+        text: course.title
+      }).appendTo($course);
+
+      $('<img />', {
+        src: course.badge
+      }).appendTo($course);
+
+      $('<a />', {
+        'class': 'btn btn-primary',
+        target: '_blank',
+        href: course.url,
+        text: 'See Course'
+      }).appendTo($course)
+
+    })
+
+  }
 
 });
