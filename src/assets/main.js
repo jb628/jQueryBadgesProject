6dd12bf3d4571023ea097ba8c551244f4ece9ac4
jb628 <jon.buch3@gmail.com>
2018-01-04 23:31:47 -0500

    fix url ending

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,9 +1,9 @@
 $(function() {
   $.ajax({
-    url:'https://www.codeschool.com/users/jb628',
+    url:'https://www.codeschool.com/users/jb628.json',
     dataType: 'jsonp',
     success: function (response) {
-      
+
     }
   });
   // your code will go here
