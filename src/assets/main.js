49a6ff22226692328c7178b2f896951441fd07cb
jb628 <jon.buch3@gmail.com>
2018-01-04 23:25:36 -0500

    complete task 5

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,11 @@
 $(function() {
-
+  $.ajax({
+    url:'https://www.codeschool.com/users/jb628',
+    dataType: 'jsonp',
+    success: function (response) {
+      
+    }
+  });
   // your code will go here
 
 });
